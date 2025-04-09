af3b9a6f1ca0a90f165638c20432d4535d99f060
tulegenovan <tulegenovan@berea.edu>
2025-04-09 11:37:33 -0400

    Added a timer, improved the game loop logic

--- turtle.py
+++ turtle.py
@@ -10,18 +10,17 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 turtle_image = pygame.image.load('image/turtle.png').convert_alpha()
+turtle_image = pygame.transform.scale(turtle_image, (100, 100))
 turtle_image.set_colorkey((255, 255, 255), pygame.RLEACCEL)
-
-
 
 
 class TurtleObject:
 
     def __init__(self):
-        self.size = 50
+        self.size = 100
         self.image = turtle_image
-        self.x = random.randint(0, screen_width)
-        self.y = random.randint(0, screen_height)
+        self.x = 0
+        self.y = 0
         self.visible = True
 
         self.last_move_time = time.time() # save current time
@@ -49,27 +48,41 @@ class TurtleObject:
              self.move()
 
 
+
 game_turtle = TurtleObject()
 score = 0
-start_time = time.time
+start_time = time.time()
 duration = 20
+
+def game_timer(start_time, duration = 20):
+    """Check if the timer has expired."""
+    current_time = time.time()
+    elapsed = current_time - start_time
+    return elapsed >= duration
+
+
 # game loop
 running = True
-
-screen.fill((0, 0, 0))  # Clear the screen with black
-
-    # Draw the game turtle
-game_turtle.draw(screen)
-
-    # Update the screen
-pygame.display.flip()
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
+            if game_turtle.is_clicked(mouse_pos):
+                score+=1
+                game_turtle.move()
+
+    game_turtle.update()
+    screen.fill((0, 0, 0))  # Clear the screen with black
+    game_turtle.draw(screen)
+
+    if game_timer(start_time, duration):
+        print("Time's up")
+        running = False
+
+    pygame.display.flip()
+
 
 
 # Quit pygame
