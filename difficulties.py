1451ec64e5f1e7fb48ec64efe13544b5cdd98870
phuyelk <phuyelk@berea.edu>
2025-04-21 10:54:15 -0400

    made the 2 different difficulties

--- difficulties.py
+++ difficulties.py
@@ -6,6 +6,6 @@ def get_difficulty():
         if choice == "normal":
             return 1.0  # Normal turtle speed
         elif choice == "hard":
-            return 0.5  # Fast turtle
+            return 0.5
         else:
             print("Invalid choice. Please enter, Normal, or Hard.")
