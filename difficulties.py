db6147a292b545e30e1485de38e9154b6e525714
phuyelk <phuyelk@berea.edu>
2025-04-21 10:53:38 -0400

    made the 3 different difficulties for the game.

--- difficulties.py
+++ difficulties.py
@@ -1,13 +1,11 @@
 def get_difficulty():
-    print("Choose difficulty: Easy, Medium, or Hard")
+    print("Choose difficulty: Normal, or Hard")
 
     while True:
         choice = input("Enter difficulty: ").lower()
-        if choice == "easy":
-            return 1.5  # Slower turtle
-        elif choice == "medium":
+        if choice == "normal":
             return 1.0  # Normal turtle speed
         elif choice == "hard":
             return 0.5  # Fast turtle
         else:
-            print("Invalid choice. Please enter Easy, Medium, or Hard.")
+            print("Invalid choice. Please enter, Normal, or Hard.")
